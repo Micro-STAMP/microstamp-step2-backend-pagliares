254d850f836a7fb361c877913d959f091a9a3aa4
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2024-07-24 04:29:11 -0300

    Created ResponsibilityDTO and build save Responsibility REST API

---
+++ src/main/java/microstamp/stpa/step2/service/ResponsibilityService.java
@@ -0,0 +1,8 @@
+package microstamp.stpa.step2.service;
+
+import microstamp.stpa.step2.dto.ResponsibilityDto;
+
+public interface ResponsibilityService {
+    ResponsibilityDto saveResponsibility(ResponsibilityDto responsibilityDto);
+
+}
---
+++ src/main/java/microstamp/stpa/step2/service/impl/ResponsibilityServiceImpl.java
@@ -0,0 +1,38 @@
+package microstamp.stpa.step2.service.impl;
+
+import lombok.AllArgsConstructor;
+
+import microstamp.stpa.step2.dto.ResponsibilityDto;
+import microstamp.stpa.step2.entity.Responsibility;
+import microstamp.stpa.step2.repository.ResponsibilityRepository;
+import microstamp.stpa.step2.service.ResponsibilityService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ResponsibilityServiceImpl implements ResponsibilityService {
+
+    private ResponsibilityRepository responsibilityRepository;
+
+    @Override
+    public ResponsibilityDto saveResponsibility(ResponsibilityDto responsibilityDto) {
+        Responsibility responsibility = new Responsibility(
+                responsibilityDto.getId(),
+                responsibilityDto.getResponsibilityCode(),
+                responsibilityDto.getResponsibilityDescription(),
+                responsibilityDto.getSystemSafetyConstraintCode()
+        );
+
+        Responsibility savedResponsibility = responsibilityRepository.save(responsibility);
+
+        ResponsibilityDto savedResponsibilityDto = new ResponsibilityDto(
+                savedResponsibility.getId(),
+                savedResponsibility.getResponsibilityCode(),
+                savedResponsibility.getResponsibilityDescription(),
+                savedResponsibility.getSystemSafetyConstraintCode()
+        );
+
+        return savedResponsibilityDto;
+
+    }
+}
