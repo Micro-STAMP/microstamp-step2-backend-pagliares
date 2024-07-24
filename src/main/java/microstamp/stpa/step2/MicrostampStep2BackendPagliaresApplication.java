208ba4011591ba78be9f5a7d9d8cd28dc94af08b
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2024-07-24 03:11:16 -0300

    Create Spring Boot project for STPA Step2 microservice with Spring Initializr

---
+++ src/main/java/microstamp/stpa/step2/MicrostampStep2BackendPagliaresApplication.java
@@ -0,0 +1,13 @@
+package microstamp.stpa.step2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrostampStep2BackendPagliaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrostampStep2BackendPagliaresApplication.class, args);
+	}
+
+}
