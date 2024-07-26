a232478d403e5caabd2cfc2f3ff9683bad613d18
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2024-07-25 21:00:44 -0300

    Update OpenFeignAPIClient interface to use service name instead of hard-code the URL and port

--- src/main/java/microstamp/stpa/step2/service/OpenFeignAPIClient.java
+++ src/main/java/microstamp/stpa/step2/service/OpenFeignAPIClient.java
@@ -7,7 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url="http://localhost:8080", value="microstamp-step1-backend-pagliares")
+//@FeignClient(url="http://localhost:8080", value="microstamp-step1-backend-pagliares")
+@FeignClient(name="STEP1-MICROSERVICE-PAGLIARES")
+
 public interface OpenFeignAPIClient {
     // Get SystemSafetyConstraint REST API
     @GetMapping("api/system-safety-constraints/{system-safety-constraint-code}")
